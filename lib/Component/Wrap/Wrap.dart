c35e74616946e2528ea08a283767448c8aaf760c
edz <edz@edzdeMacBook-Air-3.local>
2020-02-26 18:25:38 +0800

    feat: Wrap组件

--- lib/Component/Wrap/Wrap.dart
+++ lib/Component/Wrap/Wrap.dart
@@ -7,6 +7,12 @@ import 'package:flutter/material.dart';
 
    一般在row中横向多个button排列，超出屏幕会报错，
    wrap组件则是会将其换行处理
+
+   属性
+
+  横向的排列方式
+  alignment: WrapAlignment.end, 在父组件的右边
+  runAlignment:WrapAlignment.end 父组件的下边排列
 */
 
 class MyWrap extends StatelessWidget {
@@ -14,42 +20,48 @@ class MyWrap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Wrap(
-      spacing: 10.0,
-      runSpacing: 10.0,
-      alignment: WrapAlignment.end,
-      children: <Widget>[
-        new RaisedButton(
-          child: Text("这是一个按钮"),
-          textColor: Theme.of(context).accentColor,
-          onPressed: () {},
-        ),
-        new RaisedButton(
-          child: Text("这是一个按钮"),
-          textColor: Theme.of(context).accentColor,
-          onPressed: () {},
-        ),
-        new RaisedButton(
-          child: Text("这是一个按钮"),
-          textColor: Theme.of(context).accentColor,
-          onPressed: () {},
-        ),
-        new RaisedButton(
-          child: Text("这是一个按钮"),
-          textColor: Theme.of(context).accentColor,
-          onPressed: () {},
-        ),
-        new RaisedButton(
-          child: Text("这是一个按钮"),
-          textColor: Theme.of(context).accentColor,
-          onPressed: () {},
-        ),
-        new RaisedButton(
-          child: Text("这是一个按钮"),
-          textColor: Theme.of(context).accentColor,
-          onPressed: () {},
-        ),
-      ],
-    );
+    return new Container(
+        height: 300.0,
+        width: 300.0,
+        color: Colors.red,
+        child: new Wrap(
+          spacing: 10.0,
+          runSpacing: 10.0,
+          // 横向的排列方式
+          alignment: WrapAlignment.end,
+          runAlignment: WrapAlignment.end,
+          children: <Widget>[
+            new RaisedButton(
+              child: Text("这是一个按钮1"),
+              textColor: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            new RaisedButton(
+              child: Text("这是一个按钮2"),
+              textColor: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            new RaisedButton(
+              child: Text("这是一个按钮3"),
+              textColor: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            new RaisedButton(
+              child: Text("这是一个按钮4"),
+              textColor: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            new RaisedButton(
+              child: Text("这是一个按钮5"),
+              textColor: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+            new RaisedButton(
+              child: Text("这是一个按钮6"),
+              textColor: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
+          ],
+        ));
   }
 }
